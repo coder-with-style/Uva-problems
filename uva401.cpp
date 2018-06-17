b48a91139c7177f81a9a44046793348c833bd7b9
coder-with-style <coderWithStyle@gmail.com>
2018-06-17 18:48:16 +0900

    401 - Palindromes
    Palindromes & Mirrored strings

--- uva401.cpp
+++ uva401.cpp
@@ -20,19 +20,27 @@ const double pi = 3.14159265358979323846;
 const int INF = 0x3f3f3f3f;
 const int X10=1024,X11=2048,X12=4096,X13=8196,X14=16392,X15=32786,X16=65536,X17=131072,X18=262144,X19=524288,X20=1048576;
 
-int A[6][4]={{2,3,4,5},{3,2,5,4},{1,0,2,2},{0,1,3,3},{4,4,1,0},{5,5,0,1}};
+map<char,char> mp={{'A','A'},{'E','3'},{'H','H'},{'I','I'},{'J','L'},{'L','J'},{'M','M'},{'O','O'},{'S','2'},
+                   {'T','T'},{'U','U'},{'V','V'},{'W','W'},{'X','X'},{'Y','Y'},{'Z','5'},{'1','1'},{'2','S'},
+                   {'3','E'},{'5','Z'},{'8','8'}};
+bool is_mirror(string s){
+    for (int i = 0; i < s.size(); ++i) {
+        if(s[i]!=mp[s[s.size()-i-1]])return false;
+    }
+    return true;
+}
+
+bool is_palindrome(string s){
+    for (int i = 0; i < s.size(); ++i) {
+        if(s[i]!=s[s.size()-i-1])return false;
+    }
+    return true;
+}
 void _(){
-    int n;
-    while (cin >> n &&n){
-        int t=0;
-        for (int i = 0; i < n-1; ++i) {
-            string s;
-            cin >> s;
-            if(s!="No"){
-                t=A[t][s=="+y"?0:s=="-y"?1:s=="+z"?2:3];
-            }
-        }
-        cout <<( t==0?"+x":t==1?"-x":t==2?"+y":t==3?"-y":t==4?"+z":"-z") <<"\n";
+    string s;
+    while (cin >> s){
+        if(is_mirror(s))cout << s << " -- is" <<(is_palindrome(s)? " a mirrored palindrome." :" a mirrored string.") <<"\n\n";
+        else cout << s << " -- is" <<(is_palindrome(s)? " a regular palindrome." :" not a palindrome.") <<"\n\n";
     }
 
 }
